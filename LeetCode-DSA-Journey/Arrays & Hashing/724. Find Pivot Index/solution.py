8a088ddd027c5a0ccc7c516426b962231ccf90a5
Vlad X <124778804+vaibhavsss@users.noreply.github.com>
2025-07-25 00:04:00 +0530

    Delete LeetCode-DSA-Journey directory

--- LeetCode-DSA-Journey/Arrays & Hashing/724. Find Pivot Index/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        total = sum(nums)
-        left_sum = 0
-        for i, x in enumerate(nums):
-            if left_sum == total - left_sum - x:
-                return i
-            left_sum += x
-        return -1
